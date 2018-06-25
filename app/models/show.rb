f72e17264dded5c5c6f01088b5db736dbb9b75c3
Sean Altemose <sean@seanaltemose.com>
2018-06-25 21:04:31 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -21,4 +21,10 @@ class Show < ActiveRecord::Base
 
   def self.popular_shows
     Show.where("rating > ?", 5)
+  end
+
+  def self.shows_by_alphabetical_order
+    Show.order(title:)
+  end
+  
 end
